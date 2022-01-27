c9bb1dba1854b7e431b8f21902715712dbc932c5
Hector Wong <hectorwong2110@gmail.com>
2022-01-26 21:57:43 -0700

    Arreglando importacione del layout

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../../components/layout';
+import Layout from '../../components/Layout';
 import Image from 'next/image';
 
 export default function Blog(){
