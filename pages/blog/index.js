f0492ed0e691d278f6aad11c488d666ea74c282f
Hector Wong <hectorwong2110@gmail.com>
2022-01-26 21:48:13 -0700

    Primer Commit

---
+++ pages/blog/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../../components/layout';
+import Image from 'next/image';
+
+export default function Blog(){
+  return (
+      <Layout>
+          <p>Hola</p>
+      </Layout>
+  );
+};
+
+
---
+++ pages/proyectos/index.js
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import Layout from "../../components/layout";
+
+export default function Proyectos({data}){
+    return(
+        <Layout>
+            {data.map(({id,name,email,body})=>(
+            <div key={id}>
+              
+                <Link href={`/proyectos/${id}`}>
+                <a>
+                <h3>{id} -- {name} -- {email} </h3>
+                </a>
+                </Link>
+                 
+              <p> {body} </p>
+            </div>
+          ))}
+        </Layout>
+    );
+}
+
+export async function getStaticProps(){
+    try {
+      const res=await fetch('https://jsonplaceholder.typicode.com/comments');
+      const data= await res.json();
+  
+      return{
+        props:{data}
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  } 
